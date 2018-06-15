1324f131fb1935c8c1156225d2fbffc5395eb6f1
marianacaramico <marianacaramico@hotmail.com>
2018-06-15 20:45:41 -0300

    Create Preferencia.cs

---
+++ ProvaSemestral/Preferencia.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProvaSemestral.Models
+{
+    public class Preferencia
+    {
+        public int IdPreferencia { get; set; }
+        public int IdUsuario { get; set; }
+        public int Valor { get; set; }
+
+    }
+}
