b1f6938089b21c3f1ac63e1b6c6ad7ebed6d34d1
marianacaramico <marianacaramico@hotmail.com>
2018-06-15 20:46:03 -0300

    Create Usuario.cs

---
+++ ProvaSemestral/Usuario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProvaSemestral.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
